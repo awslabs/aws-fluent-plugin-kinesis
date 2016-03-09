25adfad9fdcc46adb19cac1e6ad1a070a58b3fb7
Ryosuke IWANAGA <riywo.jp@gmail.com>
2016-03-10 03:40:25 +0900

    Fix travis

--- lib/fluent/plugin/kinesis_helper.rb
+++ lib/fluent/plugin/kinesis_helper.rb
@@ -12,7 +12,6 @@
 #  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
-require 'fluent/process'
 require 'fluent/plugin/kinesis_helper/class_methods'
 require 'fluent/plugin/kinesis_helper/initialize'
 
--- test/test_out_kinesis_producer.rb
+++ test/test_out_kinesis_producer.rb
@@ -12,6 +12,7 @@
 #  express or implied. See the License for the specific language governing
 #  permissions and limitations under the License.
 
+require 'fluent/load'
 require 'fluent/test'
 require 'fluent/plugin/out_kinesis_producer'
 
