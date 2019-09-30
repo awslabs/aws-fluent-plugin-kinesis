3f4e2d36a02a18aaa7f7a9ec76816583292aefbb
Steven D Bruggeman <steve.bruggeman@gmail.com>
2019-09-30 10:12:56 -0500

    modified sts client to support using sts endpoint url and sts proxy together.
    reverted sts client to support only a region parameter.

--- lib/fluent/plugin/kinesis_helper/client.rb
+++ lib/fluent/plugin/kinesis_helper/client.rb
@@ -128,7 +128,9 @@ module Fluent
             credentials_options[:duration_seconds] = c.duration_seconds if c.duration_seconds
             credentials_options[:external_id] = c.external_id if c.external_id
             credentials_options[:sts_endpoint_url] = c.sts_endpoint_url if c.sts_endpoint_url
-            if c.sts_http_proxy and @region
+            if c.sts_http_proxy and c.sts_endpoint_url
+                credentials_options[:client] = Aws::STS::Client.new(http_proxy: c.sts_http_proxy, endpoint: c.sts_endpoint_url)
+            elsif c.sts_http_proxy and @region
                 credentials_options[:client] = Aws::STS::Client.new(region: @region, http_proxy: c.sts_http_proxy)
             elsif @region and c.sts_endpoint_url
                 credentials_options[:client] = Aws::STS::Client.new(region: @region, endpoint: c.sts_endpoint_url)
@@ -136,6 +138,8 @@ module Fluent
                 credentials_options[:client] = Aws::STS::Client.new(http_proxy: c.sts_http_proxy)
             elsif c.sts_endpoint_url
                 credentials_options[:client] = Aws::STS::Client.new(endpoint: c.sts_endpoint_url)
+            elsif @region
+                credentials_options[:client] = Aws::STS::Client.new(region: @region)
             end
             options[:credentials] = Aws::AssumeRoleCredentials.new(credentials_options)
           when @instance_profile_credentials
